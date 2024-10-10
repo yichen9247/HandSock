23b8dfb95bd229e3784cb4a75c503b6a6f469bb7
yichen9247 <865252486@qq.com>
2024-10-10 19:11:11 +0800

    add err console

---
+++ src/scripts/developer.js
@@ -0,0 +1,16 @@
+
+/**
+ * name：disable-devtool
+ * document：https://github.com/topics/disable-devtool
+ */
+
+export default {
+    url: '/',
+    clearLog: true,
+    timeOutUrl: '/',
+    disableCut: false,
+    disableMenu: true,
+    disableCopy: false,
+    disablePaste: false,
+    disableIframeParents: true,
+}
