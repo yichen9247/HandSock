e81795424392af17c3b8ea9b9c1ca0eddfdb1517
yichen9247 <865252486@qq.com>
2024-10-10 19:33:56 +0800

    add err console

--- server/database.mjs
+++ server/database.mjs
@@ -3,7 +3,9 @@ export default {
     port: 3306,
     user: 'root',
     host: 'localhost',
+    charset: 'utf8mb4',
     password: '12345678',
     database: 'handsock',
-    useConnectionPooling: true
+    useConnectionPooling: true,
+    collation: 'utf8mb4_unicode_ci'
 }
