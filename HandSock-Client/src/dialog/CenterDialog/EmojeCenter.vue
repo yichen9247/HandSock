f7794a381d417cb5c8819c462f289cf0a2e80044
yichen9247 <865252486@qq.com>
2024-11-12 21:16:15 +0800

    update dependencies

--- HandSock-Client/src/dialog/CenterDialog/EmojeCenter.vue
+++ HandSock-Client/src/dialog/CenterDialog/EmojeCenter.vue
@@ -21,7 +21,7 @@
 </script>
 
 <template>
-    <el-dialog class="emoje-center" v-model="onelDialogStore.emojeListCenter" :width="applicationStore.isDeviceMobile ? '95%' : '500px'" title="键入表情" align-center destroy-on-close center>
+    <el-dialog class="emoje-center" v-model="onelDialogStore.emojeListCenter" :width="applicationStore.isDeviceMobile ? '95%' : '500px'" title="键入表情" align-center destroy-on-close center >
         <div class="emoje-list">
             <span class="emoje-item" v-for="(item, index) in emojeList" :key="index" @click="renderEmoje(item)">{{ item }}</span>
         </div>
